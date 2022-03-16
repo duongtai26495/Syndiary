7834af99e52e60d5b8217402ec7c6edb7636a2ec
duongtai26495 <taiduong.264@gmail.com>
2022-03-16 12:18:13 +0700

    Create sourcebase

---
+++ src/main/java/com/duongtai/sydiary/ApplicationSyncDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.duongtai.sydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSyncDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSyncDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duongtai/sydiary/ApplicationSyncDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duongtai.sydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSyncDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
