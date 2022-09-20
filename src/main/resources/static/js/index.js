a65387f3650b9b733b3d11b8a7c105d1bd3dc42f
duongtai26495 <taiduong.264@gmail.com>
2022-09-20 22:55:11 +0700

    update

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,38 +1,39 @@
+	
+	let app_name = "Syndiary";
+	document.getElementById('logo_heading_desktop').innerHTML = app_name.toUpperCase();
+	document.getElementById('logo_heading_mobile').innerHTML = app_name.toUpperCase();
+	
+	const diary_add = document.getElementById("diary_add");
+	const menu_drawer = document.getElementById("menu_drawer");
+	const close_menu = document.getElementById("close_menu");
+	const openMenu = document.getElementById("toggle_menu");
+	const add_new = document.getElementById("add_new");
+	openMenu.addEventListener("click",()=>{
+		menu_drawer.style.left = '0%';
+	})
+	close_menu.addEventListener("click",()=>{
+		menu_drawer.style.left = '-100%';
+	})
+	
+	
+	add_new.addEventListener("click",()=>{
+		diary_add.style.right = '0%';
+	})
 
-let app_name = "Syndiary";
-document.getElementById('logo_heading_desktop').innerHTML = app_name.toUpperCase();
-document.getElementById('logo_heading_mobile').innerHTML = app_name.toUpperCase();
 
-
-function openMenu() {
-	document.getElementById("menu_drawer").style.left = '0%';
-}
-function closeMenu(){
-	document.getElementById("menu_drawer").style.left = '-100%';
-}
-//
-//
-//function login(){
-//    let username = document.getElementById("username_login").value;
-//    let password = document.getElementById("password_login").value;
-//
-//    console.log("Access_token: "+localStorage.getItem("access_token"));
-//
-//    var formdata = new FormData();
-//    formdata.append("username", username);
-//    formdata.append("password", password);
-//
-//    var requestOptions = {
-//      method: 'POST',
-//      body: formdata,
-//      redirect: 'follow'
-//    };
-//
-//    fetch("http://192.168.1.15:8080/auth/login", requestOptions)
-//      .then(response => response.json())
-//      .then(result => localStorage.setItem("access_token",result.access_token))
-//      .catch(error => console.log('error', error));
-//
-//    console.log("Access_token: "+localStorage.getItem("access_token"));
-//}
-//
+	const accordionContent = document.querySelectorAll(".accordion_content");
+	accordionContent.forEach((item, index)=>{
+		let header = item.querySelector("header");
+		header.addEventListener("click",()=>{
+			item.classList.toggle("open");
+			
+			let content = item.querySelector(".content");
+			if(item.classList.contains("open")){
+				content.style.height = `${content.scrollHeight}px`;
+				content.style.marginTop = '10px'; 
+			}else{
+				content.style.height = '0px';
+			}
+			console.log(content);
+		})
+	})
